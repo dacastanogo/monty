ec164cd7a7fa0fc75b4b139a40175d1a30209e22
dacastanogo <dcastanogomez@gmail.com>
2019-09-26 19:50:09 -0500

    Added functions, main and header

---
+++ stack_functions_2.c
@@ -0,0 +1,72 @@
+#include "monty.h"
+
+/**
+ * m_swap - swap top two elements of `stack'
+ * @stack: double pointer to head of stack
+ * @line_number: line number of current operation
+ *
+ * Return: void
+ */
+void m_swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *next;
+
+	if (var.stack_len < 2)
+	{
+		dprintf(STDOUT_FILENO,
+			"L%u: can't swap, stack too short\n",
+			line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (var.stack_len == 2)
+	{
+		*stack = (*stack)->next;
+		return;
+	}
+	next = (*stack)->next;
+	next->prev = (*stack)->prev;
+	(*stack)->prev->next = next;
+	(*stack)->prev = next;
+	(*stack)->next = next->next;
+	next->next->prev = *stack;
+	next->next = *stack;
+	*stack = next;
+}
+
+#include "monty.h"
+
+/**
+ * m_add - add the top two elements of the stack
+ * @stack: double pointer tot he beginning of the stack
+ * @line_number: script line number
+ *
+ * Return: void
+ */
+void m_add(stack_t **stack, unsigned int line_number)
+{
+	int n = 0;
+
+	if (var.stack_len < 2)
+	{
+		dprintf(STDOUT_FILENO,
+			"L%u: can't add, stack too short\n",
+			line_number);
+		exit(EXIT_FAILURE);
+	}
+	n += (*stack)->n;
+	m_pop(stack, line_number);
+	(*stack)->n += n;
+}
+
+/**
+ * m_nop - no operation performed
+ * @stack: double pointer to head of stack
+ * @line_number: line number of current operation
+ *
+ * Return: void
+ */
+void m_nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
